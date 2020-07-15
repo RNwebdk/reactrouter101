4f2824eb2de7ae3a65cae795ae9844e1c5805fdc
Rami <info@rnweb.dk>
2020-07-15 14:37:02 +0200

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Home from './Home';
+import Help from './Help';
 
 const Host = () => <h1>Host</h1>;
-const Help = () => <h1>Help</h1>;
 const Login = () => <h1>Login</h1>;
 const Signup = () => <h1>Sign up</h1>;
 
@@ -21,7 +21,7 @@ function App() {
         }}
       />
       <Route exact path='/host' component={Host} />
-      <Route exact path='/help' component={Help} />
+      <Route path='/help' component={Help} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/signup' component={Signup} />
     </Router>
