fea23875e5f02a1621bf4ca665c06279fcb17ac5
Rami <info@rnweb.dk>
2020-07-15 14:21:06 +0200

    Routes with Component vs. Render
    Testing out render in router component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
+import Home from './Home';
 
-const Home = () => <h1>Home</h1>;
 const Host = () => <h1>Host</h1>;
 const Help = () => <h1>Help</h1>;
 const Login = () => <h1>Login</h1>;
@@ -13,7 +13,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Route exact path='/' component={Home} />
+      <Route
+        exact
+        path='/'
+        render={() => {
+          return <Home title='This is Home' />;
+        }}
+      />
       <Route exact path='/host' component={Host} />
       <Route exact path='/help' component={Help} />
       <Route exact path='/login' component={Login} />
